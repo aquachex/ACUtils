ddd450e02859b1998ce96fa82e614003410513f2
aquachex <dave.lilly@gmail.com>
2013-09-26 15:37:52 -0500

    Initial commit.

---
+++ ACUtils/Categories/NSString+AC.h
@@ -0,0 +1,13 @@
+//
+//  NSString+AC.h
+//  ACUtils
+//
+//  Created by Dave Lilly on 9/26/13.
+//  Copyright (c) 2013 Dave Lilly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (AC)
+
+@end
