e02d3e7056c607fd14128f39706070dcede2a777
aquachex <dave.lilly@gmail.com>
2013-09-26 15:05:01 -0500

    Initial Commit

---
+++ ACUtils/ACUtils.h
@@ -0,0 +1,13 @@
+//
+//  ACUtils.h
+//  ACUtils
+//
+//  Created by Dave Lilly on 9/26/13.
+//  Copyright (c) 2013 Dave Lilly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACUtils : NSObject
+
+@end
---
+++ ACUtils/ACUtils.m
@@ -0,0 +1,13 @@
+//
+//  ACUtils.m
+//  ACUtils
+//
+//  Created by Dave Lilly on 9/26/13.
+//  Copyright (c) 2013 Dave Lilly. All rights reserved.
+//
+
+#import "ACUtils.h"
+
+@implementation ACUtils
+
+@end
